bb3f9f2a74fc868d743f31a1e41252393bc4c482
subhashini.dhamodaran <1910subhashini@gmail.com>
2026-03-31 08:07:55 -0400

    updated redirect urls

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'tickish-api.azurewebsites.net',
   msal: {
     // UI app registration client ID — from Entra portal (App registrations → your UI app)
     clientId: '9a47834c-659c-4b13-9acb-4b8105521f68',
     authority: 'https://todoappusers.ciamlogin.com/bc4899c5-a308-4874-b570-d33b01a4d30d',
-    redirectUri: 'http://localhost:4200/redirect',
-    postLogoutRedirectUri: 'http://localhost:4200',
+    redirectUri: 'https://polite-stone-08cded10f.4.azurestaticapps.net/redirect',
+    postLogoutRedirectUri: 'https://polite-stone-08cded10f.4.azurestaticapps.net',
+
   },
   // The API's client ID is used to construct the scope Angular requests
   // Format: api://{apiClientId}/{scopeName}
